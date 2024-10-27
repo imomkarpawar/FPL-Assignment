18ac9e4f5413ae984814381cefba7ee67f65abb8
Omkar Pawar <omkarpawar4824@gmail.com>
2024-10-27 17:06:33 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+// Program for Arithematic Operator
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 20;
+        printf("%d\n", a + b); // Addition
+        printf("%d\n", a - b); // Substraction
+        printf("%d\n", a * b); // Multiplication
+        printf("%d\n", a / b); // Division
+        printf("%d\n", a % b); // Modulus
+    return 0;
+}
